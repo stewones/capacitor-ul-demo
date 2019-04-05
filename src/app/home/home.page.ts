9d33d1aefa496ddd9413169ee9d324a33ea0ee68
Stewan <oi@stewan.io>
2019-04-05 14:07:30 -0300

    chore(): add ul demo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Plugins, AppUrlOpen } from "@capacitor/core";
+const { App } = Plugins;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  data: AppUrlOpen = <AppUrlOpen>{};
+  ngOnInit() {
+    App.addListener("appUrlOpen", (data: AppUrlOpen) => {
+      alert(`data: ${JSON.stringify(data)}`);
+      this.data = data;
+    });
+  }
 }
